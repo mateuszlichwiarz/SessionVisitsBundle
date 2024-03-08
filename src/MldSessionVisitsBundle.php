6d2bfbc6e19a51e613862a8fe8221fd618c2d355
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-03-08 04:58:25 +0100

    implement visits session extension in bundle class

--- src/MldSessionVisitsBundle.php
+++ src/MldSessionVisitsBundle.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace MLD\SessionVisitsBundle;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+use MLD\SessionVisitsBundle\DependencyInjection\MldSessionVisitsExtension;
 
 class MldSessionVisitsBundle extends AbstractBundle
 {
     public function getPath(): string
     {
         return dirname(path:__DIR__);
+    }
+
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new MldSessionVisitsExtension();
     }
 }
