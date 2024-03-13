bd018fb973f089dcb751f9d35ac33222c8738118
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-03-13 04:57:35 +0100

    create session visits controller

---
+++ src/Controller/SessionVisitsController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MLD\SessionVisitsBundle\Controller;
+
+use MLD\SessionVisitsBundle\VisitRegister\VisitsRegister;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SessionVisitsController extends AbstractController
+{
+    public function __construct(private VisitsRegister $visitsRegister)
+    {}
+
+    public function __invoke()
+    {
+        return $this->visitsRegister->saveVisit();
+    }
+}
