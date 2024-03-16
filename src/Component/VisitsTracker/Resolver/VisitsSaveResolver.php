2f63f134600a57187c172c4c0e46fc76401dcec7
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-03-16 03:14:38 +0100

    refactor: change name and shorter if in save method

---
+++ src/Component/VisitsTracker/Resolver/VisitsSaveResolver.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MLD\SessionVisitsBundle\VisitsTracker\Resolver;
+
+use MLD\SessionVisitsBundle\VisitsTracker\Factory\VisitsFactory;
+use MLD\SessionVisitsBundle\VisitsTracker\Updater\VisitsUpdater;
+use MLD\SessionVisitsBundle\VisitsTracker\Persister\VisitsPersister;
+use MLD\SessionVisitsBundle\Repository\VisitsRepository;
+use MLD\SessionVisitsBundle\Entity\Visits;
+
+use MLD\SessionVisitsBundle\Entity\Date;
+use MLD\SessionVisitsBundle\Component\DateSystem\Factory\CurrentDateFactory;
+
+class VisitsSaveResolver
+{
+    private Date $date;
+
+    private ?Visits $visits = null;
+
+    public function __construct(
+        private VisitsRepository $visitsRepository,
+        private VisitsFactory $visitsFactory,
+        private VisitsUpdater $visitsUpdater,
+        private VisitsPersister $visitsPersister,
+        private CurrentDateFactory $currentDateFactory,
+    ){
+        $this->date = $this->currentDateFactory->create();
+        $this->visits = $this->findVisit();
+    }
+
+    public function save(): void
+    {
+        $this->visitsPersister->persist($this->visits === null 
+            ? $this->visitsFactory->create($this->date)
+            : $this->visitsUpdater->update($this->visits)
+        );
+    }
+
+    private function findVisit(): ?Visits
+    {
+        return $this->visitsRepository->findOneVisitsObjectByDate($this->date);
+    }
+}
