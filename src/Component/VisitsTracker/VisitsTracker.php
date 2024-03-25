f5018e0057fc388700f6abd47120575d11cf0894
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-03-25 23:10:24 +0100

    fix: update tracker class on new named resolver

--- src/Component/VisitsTracker/VisitsTracker.php
+++ src/Component/VisitsTracker/VisitsTracker.php
@@ -5,20 +5,20 @@ declare(strict_types=1);
 namespace MLD\SessionVisitsBundle\Component\VisitsTracker;
 
 use MLD\SessionVisitsBundle\Component\VisitsTracker\Session\SessionRegister;
-use MLD\SessionVisitsBundle\Component\VisitsTracker\Resolver\VisitsSaveResolver;
+use MLD\SessionVisitsBundle\Component\VisitsTracker\Resolver\NewOrUpdateVisitsResolver;
 
 class VisitsTracker
 {
     public function __construct(
         private SessionRegister $sessionRegister,
-        private VisitsSaveResolver $visitsSaveResolver,
+        private NewOrUpdateVisitsResolver $visitsResolver,
     ){}
 
     public function start(): void
     {
         if($this->sessionRegister->IsSessionRegistered() === null) {
-            $this->visitsSaveResolver->save();
             $this->sessionRegister->register();
+            $this->visitsResolver->resolve();
         }
     }
 
