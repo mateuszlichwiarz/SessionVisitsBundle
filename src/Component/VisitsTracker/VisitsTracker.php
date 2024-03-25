ca755d5482feb0af32921ee71d148bcc25e33c2d
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-03-25 23:54:17 +0100

    refactor: move some responsibilty to visits tracker main class from resolver

--- src/Component/VisitsTracker/VisitsTracker.php
+++ src/Component/VisitsTracker/VisitsTracker.php
@@ -5,20 +5,37 @@ declare(strict_types=1);
 namespace MLD\SessionVisitsBundle\Component\VisitsTracker;
 
 use MLD\SessionVisitsBundle\Component\VisitsTracker\Session\SessionRegister;
-use MLD\SessionVisitsBundle\Component\VisitsTracker\Resolver\NewOrUpdateVisitsResolver;
+use MLD\SessionVisitsBundle\Component\VisitsTracker\Resolver\NewOrAddVisitsResolver;
+use MLD\SessionVisitsBundle\Component\VisitsTracker\Persister\VisitsPersister;
+use MLD\SessionVisitsBundle\Component\DateSystem\Factory\CurrentDateFactory;
+use MLD\SessionVisitsBundle\Repository\VisitsRepository;
+
+use MLD\SessionVisitsBundle\Entity\Visits;
+use MLD\SessionVisitsBundle\Entity\Date;
 
 class VisitsTracker
 {
+    private Date $date;
+
+    private ?Visits $visits = null;
+
     public function __construct(
         private SessionRegister $sessionRegister,
-        private NewOrUpdateVisitsResolver $visitsResolver,
-    ){}
+        private NewOrAddVisitsResolver $resolver,
+        private VisitsRepository $repository,
+        private VisitsPersister $persister,
+        private CurrentDateFactory $currentDateFactory,
+    ){
+        $this->date = $this->currentDateFactory->create();
+        $this->visits = $this->repository->findOneVisitsObjectByDate($this->date);
+        
+    }
 
     public function start(): void
     {
         if($this->sessionRegister->IsSessionRegistered() === null) {
             $this->sessionRegister->register();
-            $this->visitsResolver->resolve();
+            $this->persister->persist($this->resolver->resolve($this->visits, $this->date));
         }
     }
 
